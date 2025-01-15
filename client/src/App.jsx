c4d284544a1a0a7ed32aedcc3fb6819ff5ba7a28
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2025-01-15 18:59:19 +0530

    added images folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,46 +8,47 @@ import { Toaster } from "react-hot-toast";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import ProtectedRoute from "./components/layout/ProtectedRoute";
+// import { ThemeProvider } from "./components/ui/theme-provider";
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col h-screen overflow-hidden">
-        <Navbar />
-        <Toaster />
-        <div className="flex-grow overflow-hidden">
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/settings" element={<SettingsPage />} />
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <HomePage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/chat"
-              element={
-                <ProtectedRoute>
-                  <ChatPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute>
-                  <ProfilePage />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
+      <Router>
+        <div className="flex flex-col h-screen overflow-hidden">
+          <Navbar />
+          <Toaster />
+          <div className="flex-grow overflow-hidden">
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/settings" element={<SettingsPage />} />
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <HomePage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/chat"
+                element={
+                  <ProtectedRoute>
+                    <ChatPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <ProfilePage />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
   );
 };
 
